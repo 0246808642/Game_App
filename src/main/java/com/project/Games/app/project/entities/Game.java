12fc9343ab59c16e299f7aeaa15af495ef57723b
Caique Coelho Pinheiro <caiquepinheiro69@gmail.com>
2025-05-21 21:08:00 -0300

    Game seed

--- src/main/java/com/project/Games/app/project/entities/Game.java
+++ src/main/java/com/project/Games/app/project/entities/Game.java
@@ -16,27 +16,106 @@ public class Game {
         @Column(name = "game_year")
         private Integer year;
         private String genre;
-        private String platform;
+        private String platforms;
+        private Double score;
         private String imgUrl;
+
+        @Column(columnDefinition = "TEXT")
         private String shortDescription;
+
+        @Column(columnDefinition = "TEXT")
         private String longDescription;
+
 
         public Game() {
 
         }
 
-        public Game(String genre, Long id, String imgUrl, String longDescription, String platform, String shortDescription, String title, Integer year) {
-            this.genre = genre;
-            this.id = id;
-            this.imgUrl = imgUrl;
-            this.longDescription = longDescription;
-            this.platform = platform;
-            this.shortDescription = shortDescription;
-            this.title = title;
-            this.year = year;
-        }
+    public Game(String genre, Long id, String imgUrl, String longDescription, String platforms, Double score, String shortDescription, String title, Integer year) {
+        this.genre = genre;
+        this.id = id;
+        this.imgUrl = imgUrl;
+        this.longDescription = longDescription;
+        this.platforms = platforms;
+        this.score = score;
+        this.shortDescription = shortDescription;
+        this.title = title;
+        this.year = year;
+    }
 
-        @Override
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    public String getPlatform() {
+        return platforms;
+    }
+
+    public void setPlatform(String platform) {
+        this.platforms = platform;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    @Override
         public boolean equals(Object o) {
             if (o == null || getClass() != o.getClass()) return false;
             Game game = (Game) o;
