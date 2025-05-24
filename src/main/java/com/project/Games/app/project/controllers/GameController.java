5ae705ce99f10f173b384b57616f5a963ac94d04
Caique Coelho Pinheiro <caiquepinheiro69@gmail.com>
2025-05-24 16:40:01 -0300

    GET game by id

--- src/main/java/com/project/Games/app/project/controllers/GameController.java
+++ src/main/java/com/project/Games/app/project/controllers/GameController.java
@@ -1,14 +1,12 @@
 package com.project.Games.app.project.controllers;
 
+import com.project.Games.app.project.dto.GameDTO;
 import com.project.Games.app.project.dto.GameMinDTO;
 import com.project.Games.app.project.entities.Game;
 import com.project.Games.app.project.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.graphql.ConditionalOnGraphQlSchema;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +20,12 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll(){
         List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO result = gameService.findById(id);
         return result;
     }
 }
--- src/main/java/com/project/Games/app/project/services/GameService.java
+++ src/main/java/com/project/Games/app/project/services/GameService.java
@@ -1,11 +1,13 @@
 package com.project.Games.app.project.services;
 
+import com.project.Games.app.project.dto.GameDTO;
 import com.project.Games.app.project.dto.GameMinDTO;
 import com.project.Games.app.project.entities.Game;
 import com.project.Games.app.project.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,13 @@ public class GameService {
 
     @Autowired
     private GameRepository gameRepository;
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(long id){
+        Game result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
+        return dto;
+    }
 
     public List<GameMinDTO> findAll(){
        List<Game> result = gameRepository.findAll();
